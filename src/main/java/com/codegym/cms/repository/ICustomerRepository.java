b971524a9a67407637b3631b1eefb8a3e0c0b478
tung1099 <dtroyal1102@gmail.com>
2022-05-05 13:54:31 +0700

    first commit

---
+++ src/main/java/com/codegym/cms/repository/ICustomerRepository.java
@@ -0,0 +1,7 @@
+package com.codegym.cms.repository;
+
+import com.codegym.cms.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ICustomerRepository extends JpaRepository<Customer, Long> {
+}
